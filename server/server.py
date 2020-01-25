b0ea5b4b01a6930988e8cf8fda5b99423e3efeb3
Andy Jiang <andy.jiang99@outlook.com>
2020-01-25 14:54:38 -0500

    ngrok flask setup

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.run()
